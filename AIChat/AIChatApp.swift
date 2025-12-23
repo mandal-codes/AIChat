32178e5497c13cfc1ba22b8b1f44e4d25d25e00e
Shubham Mandal <mandal.shubham.learn@gmail.com>
2025-12-23 05:42:10 +0530

    Initial Commit

---
+++ AIChat/AIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatApp.swift
+//  AIChat
+//
+//  Created by Shubham Mandal on 23/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChatTests/AIChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatTests.swift
+//  AIChatTests
+//
+//  Created by Shubham Mandal on 23/12/25.
+//
+
+import Testing
+@testable import AIChat
+
+struct AIChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
